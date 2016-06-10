d550de21eeff3946848c090360118fee09075174
福島達也 <t.mugen282@gmail.com>
2016-06-10 19:42:19 +0900

    initial commit

--- tangochou2/QuestionViewController.swift
+++ tangochou2/QuestionViewController.swift
@@ -30,7 +30,7 @@ class QuestionViewController: UIViewController {
     }
     
     override func viewWillAppear(animated: Bool){
-        wordArray = saveData.arrayForKey("Word")!
+        wordArray = saveData.arrayForKey("WORD")!
         
         shuffle()
         questionLabel.text = shuffledWordArray[nowNumber]["english"] as? String
